fda21981ec496d2cf0cffdeaa169f027210a99cc
aadarsh <aadarshg2562003@example.com>
2025-02-12 15:46:48 +0530

    [Aadarsh] Add . Compare two sets for equality

---
+++ setinterface/setequality/SetEquality.java
@@ -0,0 +1,15 @@
+package com.tit.week04.day02.collections.setinterface.setequality;
+import java.util.Set;
+
+public class SetEquality {
+    public static <T> boolean areSetsEqual(Set<T> set1, Set<T> set2) {
+        return set1.equals(set2);
+    }
+
+    public static void main(String[] args) {
+        Set<Integer> set1 = Set.of(1, 2, 3);
+        Set<Integer> set2 = Set.of(3, 2, 1);
+
+        System.out.println("Are sets equal? " + areSetsEqual(set1, set2));
+    }
+}
