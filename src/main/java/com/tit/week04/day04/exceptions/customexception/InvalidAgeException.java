24c851052b0ef38fdc798f6494f2e9eeed8dcded
aadarsh <aadarshg2562003@example.com>
2025-02-14 11:25:59 +0530

    [Aadarsh] Add . Added custom InvalidAgeException with age validation

---
+++ src/main/java/com/tit/week04/day04/exceptions/customexception/InvalidAgeException.java
@@ -0,0 +1,9 @@
+package com.tit.week04.day04.exceptions.customexception;
+
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+
