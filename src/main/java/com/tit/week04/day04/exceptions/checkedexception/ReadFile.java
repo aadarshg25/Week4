2cb305cd7221de23c0f004997abbd0d67837d186
aadarsh <aadarshg2562003@example.com>
2025-02-14 11:15:22 +0530

    [Aadarsh] Add . Handled checked exception for file reading

---
+++ src/main/java/com/tit/week04/day04/exceptions/checkedexception/ReadFile.java
@@ -0,0 +1,18 @@
+package com.tit.week04.day04.exceptions.checkedexception;
+
+import java.io.*;
+
+public class ReadFile{
+    public static void main(String[] args) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("Source.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("File not found");
+        }
+    }
+}
